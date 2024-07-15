a3934786c71abd1043bedc9832502bfd8a0b1ac7
Lalo64GG <223257@ids.upchiapas.edu.mx>
2024-07-14 23:26:09 -0600

    chore: Update app.ts and index.ts to remove dotenv.config()

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { appDataSources } from "./config/db.config";
 import https from 'https';
 import fs from 'fs';
 import path from 'path';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 const main = async () => {
     try {
@@ -11,8 +15,8 @@ const main = async () => {
         const PORT = process.env.PORT || 5000;
 
         const httpsOptions = {
-            key: fs.readFileSync(path.join(__dirname, '../../../etc/nginx/ssl/nginx-selfsigned.key')),
-            cert: fs.readFileSync(path.join(__dirname, '../../../etc/nginx/ssl/nginx-selfsigned.crt'))
+            key: fs.readFileSync(path.join(__dirname, '../../../../etc/nginx/ssl/nginx-selfsigned.key')),
+            cert: fs.readFileSync(path.join(__dirname, '../../../../etc/nginx/ssl/nginx-selfsigned.crt'))
         };
 
         https.createServer(httpsOptions, app).listen(PORT, () => {
